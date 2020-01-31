704022ecb8f57e7d4e00c20747eaf607df1c8980
Bruno <brunobapmon@gamil.com>
2020-01-30 21:20:26 -0300

    main endpoint and requirements file added

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from web_crawler import crawl
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+def main_message():
+    return jsonify('Welcome! Please send your requests to the /crawl with domain and depth parameters.')
+
+@app.route('/crawl', methods=['GET'])
 def get_tasks():
     try:
         domain = request.args.get('domain')
         depth = request.args.get('depth')
         return jsonify(crawl(domain, depth))
     except:
-        return jsonify("Something went wrong, please try again")
+        return jsonify('Something went wrong, please try again')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, port=5000)
